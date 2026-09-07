8a61f5459fac40f7209770cbc838192d13cf9b3b
DouglasRochaTech <douglas.rocha.tech@gmail.com>
2026-09-06 21:31:42 -0300

    Muitas mudanças...
    - Objetos organizados na hierarquia: objetos que estavam soltos na hierarquia agora estão acoplados aos objetos chamados "OBJETOS ESTÁTICOS";
    - Mudanças no terreno;
    - Aumentada a distância máxima das sombras lunares;
    - Otimizações de performance nas sombras lunares;
    - Mudança nas configurações da luz da lua, para consertar bugs visuais;
    - Consertado o problema das árvores bugando violentamente conforme seus LODs eram substituídos;
    - Consertado o problema do material rosa no LOD distante das árvores mortas;
    - Adicionado de volta o efeito "bloom";
    - Criado um script novo para culling de objetos distantes;
    - Bugs de iluminação consertados;

---
+++ Assets/YokaiTalesScripts/CullingDeObjetos.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CullingDeObjetos : MonoBehaviour
+{
+    public Transform Player;
+    private float Distance;
+
+    public GameObject[] Objetos;
+    int Checar;
+    public int DistanciaMaxima = 250;
+    public int DistanciaVerticalMaxima = 50;
+    float DiferencaDistancia;
+
+    void Start()
+    {
+		Objetos = new GameObject[transform.childCount];
+
+	    for (int i = 0; i < transform.childCount; i++)
+        {
+			Objetos[i] = transform.GetChild(i).gameObject;
+		}
+
+		//Player = GameObject.Find("Player").transform;
+    }
+
+    void FixedUpdate()
+    {
+		Checar++;
+
+		if (Checar == 4)
+	    {
+			for (int i = 0; i < transform.childCount; i++)
+            {
+				if (Objetos[i] != null)
+				{
+					Distance = Vector3.Distance(Objetos[i].transform.position, Player.position);
+
+					if (Distance > DistanciaMaxima)
+					{
+						Objetos[i].SetActive(false);
+					}
+					else
+					{
+						DiferencaDistancia = Objetos[i].transform.position.y - Player.position.y;
+
+						if ((DiferencaDistancia < -DistanciaVerticalMaxima) | (DiferencaDistancia > DistanciaVerticalMaxima))
+						{
+							Objetos[i].SetActive(false);
+						}
+						else
+						{
+							Objetos[i].SetActive(true);
+						}
+					}
+				}
+			}
+			Checar = 0;
+	    }
+    }
+}
